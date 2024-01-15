b7ad80c9b9b1c901cd4325642fc6c885a0f818eb
Everett Price <elprice11@gmail.com>
2024-01-14 23:58:29 -0800

    Added some controls

--- src/snake/snake.py
+++ src/snake/snake.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Dict
 
 import pygame
 
@@ -15,11 +16,30 @@ def run():
 
     FramePerSec = pygame.time.Clock()
 
-    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Game")
+
+    BLACK = (0, 0, 0)
+    GRAY = (127, 127, 127)
+    WHITE = (255, 255, 255)
+    YELLOW = (255, 255, 0)
+    CYAN = (0, 255, 255)
+    MAGENTA = (255, 0, 255)
+
+    controls: Dict[int, tuple] = {
+        pygame.K_RIGHT: GRAY,
+        pygame.K_LEFT: YELLOW,
+        pygame.K_UP: CYAN,
+        pygame.K_DOWN: MAGENTA,
+    }
 
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.KEYDOWN:
+                for key, val in controls.items():
+                    if event.key == key:
+                        screen.fill(val)
+                        pygame.display.update()
+            elif event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
